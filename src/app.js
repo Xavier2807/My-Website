629e13bd71ace1b146f28734e2b5df47b7deff8a
Xavier2807 <xvan94@hotmail.com>
2017-07-23 11:12:58 +0800

    [] - [Advance]

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+let pen = 1;
+let apple = 2;
+
+let ali = 'ali';
+let ahmad = 'ahmad';
+
+//let result = pen + apple;
+
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+
+console.log(add(pen,apple));
+//console.log(ali + " " + ahmad);

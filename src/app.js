2fc338952d8793b5ec7fd21e62e83c76cba9ec6d
Xavier2807 <xvan94@hotmail.com>
2017-07-23 17:16:54 +0800

    [] - [V2]

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 let pen = 1;
 let apple = 2;
 
@@ -7,11 +6,51 @@ let ahmad = 'ahmad';
 
 //let result = pen + apple;
 
-function add(num1, num2)
-{
-    return num1 + num2;
+function add(num1, num2) {
+    return num1 * num2;
 }
 
+console.log(add(pen, apple));
+console.log(add(ali, ahmad))
 
-console.log(add(pen,apple));
-//console.log(ali + " " + ahmad);
+
+
+/*function createNewHuman(name, age) {
+    let human = {
+        name: name,
+        age: age,
+
+        // function inside object
+        talk: function () {
+            return 'hello my name is ' + name + 'age is ' + age;
+        }
+    };
+
+    return human;
+}
+
+console.log(createNewHuman('abc','15').talk());*/
+
+function Login() {
+    let username = document
+        .getElementById('usr')
+        .value;
+
+    let password = document
+        .getElementById('pwd')
+        .value;
+
+    let validLoginList = [
+        { usr: 'jane', pwd: '1234' },
+        { usr: 'john', pwd: 'abc' }
+    ]
+
+    validLoginList.forEach(function (user) {
+        if (username == user.usr && password == user.pwd) {
+            isUserFond = true;
+        }
+    }
+    )
+
+    isUserFond == true ? alert("Successful Login") : alert("Fail Login");
+}
